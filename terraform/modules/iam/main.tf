c82a2391ebe721c2752bbef72df924bfc080bb22
Your GitHub Username <youremail@example.com>
2025-12-07 16:04:41 +0530

    this is a edit

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -16,10 +16,11 @@ data "aws_iam_policy_document" "codepipeline_assume" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "codepipeline_policy" {
+resource "aws_iam_role_policy_attachment" "codepipeline_full_access" {
   role       = aws_iam_role.codepipeline_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
 }
+
 
 ##############################
 # CodeBuild Role
@@ -39,10 +40,12 @@ data "aws_iam_policy_document" "codebuild_assume" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "codebuild_policy" {
+resource "aws_iam_role_policy_attachment" "codebuild_service_role" {
   role       = aws_iam_role.codebuild_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
+
+
 
 ##############################
 # CodeDeploy Role
@@ -62,7 +65,7 @@ data "aws_iam_policy_document" "codedeploy_assume" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
+resource "aws_iam_role_policy_attachment" "codedeploy_service_role" {
   role       = aws_iam_role.codedeploy_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
 }
